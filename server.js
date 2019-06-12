5097628edc00c44ef8785821936057bc50f111c4
kdublam <kdublam@gmail.com>
2019-06-12 13:01:45 -0700

    attempt to add signup/signin to page. currently not yet working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser= require("body-parser")
+// var env = require('dotenv').load();
+
 
 var db = require("./models");
 
@@ -11,6 +16,18 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
 
 // Handlebars
 app.engine(
@@ -24,8 +41,13 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require('./routes/auth.js')(app,passport);
+
 
 var syncOptions = { force: false };
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, db.user);
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
