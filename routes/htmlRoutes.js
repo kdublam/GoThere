40011351b57616f439a8ec6bffd920b0576b51f9
joshua-unseen <corpselight@gmail.com>
2019-06-17 11:52:58 -0700

    Added a skeleton /result route and some comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,14 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  // results page
+  // put the $.ajax().then() to get this inside the /api/home POST route.
+  app.get("/result", function(req, res) {
+    // get the query from the url (req.params...) and pass it to the db
+    //db.Plans.findAll({dbquery}).then(function(dbResult) {
+      res.render("result", {data: dbResult}); // this is a framework, not necessarily literal code.
+    // });
+  });
   // Load index page
 
   // app.get("/", function(req, res) {
@@ -13,13 +21,13 @@ module.exports = function(app) {
   // });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
